c1c2d402eabbe58dc948f853de27a5781b96c4b8
gnamithan <gnaganathanahalli@ucsd.edu>
2022-02-03 01:11:40 -0800

    updates files

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -4,7 +4,7 @@ import org.junit.*;
 public class SkillDemo1Test {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo1.add(3, 1));
+        assertEquals(2, SkillDemo1.add(1, 1));
     }
     
 }
